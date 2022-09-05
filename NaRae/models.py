e41d792093481544ce7183df7b7c4131f6c64bc6
Alex <ahernan2@caltech.edu>
2022-09-05 13:50:55 -0700

    added initial models

--- NaRae/models.py
+++ NaRae/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Consumer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=100)
+    business_address1 = models.CharField(max_length=100)
+    business_city = models.CharField(max_length=50)
