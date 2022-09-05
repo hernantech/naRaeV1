a73e0a7b1b8ef90dfabf7cbc9f55c6e6c7d61780
Alex <ahernan2@caltech.edu>
2022-09-05 13:53:32 -0700

    added initial models

--- NaRae/models.py
+++ NaRae/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Driver(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
