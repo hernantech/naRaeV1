a59350bb0ef94cbe3047f743e42688bd73ea58c7
Alex <ahernan2@caltech.edu>
2022-09-05 14:06:32 -0700

    added gitignore, edits to models

--- NaRae/models.py
+++ NaRae/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Driver(models.Model):
+class Couriers(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
@@ -13,3 +13,6 @@ class Business(models.Model):
     business_name = models.CharField(max_length=100)
     business_address1 = models.CharField(max_length=100)
     business_city = models.CharField(max_length=50)
+    business_inventory = Business_inventor
+
+class Business_inventory(models.Model):
--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q0fu4w)#cr2z&zx_8*r+3p##3ejsdg=-g@ega5(3!5y9$n%^c('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
