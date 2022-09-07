fccce0373bb1e18b3c8638fcda899718498b56e3
Alex <ahernan2@caltech.edu>
2022-09-07 14:51:26 -0700

    editing models, adding constructors, etc

--- NaRae/models.py
+++ NaRae/models.py
@@ -3,16 +3,23 @@ from django.db import models
 class Couriers(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    active_job = active_job()
 
 
 class Consumer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    consumer_location =
 
 class Business(models.Model):
     business_name = models.CharField(max_length=100)
     business_address1 = models.CharField(max_length=100)
     business_city = models.CharField(max_length=50)
-    business_inventory = Business_inventor
+    #business_inventory = Business_inventor
 
 class Business_inventory(models.Model):
+
+
+class active_job:
+    def __init__(self):
+        self.active = "code goes here for creating a job"
