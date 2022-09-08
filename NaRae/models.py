a0f4f6c7ccddef0b057875e6447c4a37d16fe836
Alex <ahernan2@caltech.edu>
2022-09-07 18:05:17 -0700

    (again) editing models, adding constructors, etc

--- NaRae/models.py
+++ NaRae/models.py
@@ -7,17 +7,29 @@ class Couriers(models.Model):
 
 
 class Consumer(models.Model):
+    GUID = models.IntegerField(max_length=50) #GUID is the global user ID, i.e. unique identifier
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    consumer_location =
+    def get_location(self, int):
+        print("this is where we get the location, either via location services or via the home address that they enter")
+    #def location_changed():
+    #   a function that handles a change of address midway through the delivery, due to user error, etc
 
 class Business(models.Model):
+    business_GUID = models.IntegerField(max_length=50)
     business_name = models.CharField(max_length=100)
     business_address1 = models.CharField(max_length=100)
     business_city = models.CharField(max_length=50)
-    #business_inventory = Business_inventor
+    def cancel_order(self, ):
+        print("thing")
+    #business_inventory = Business_inventory.get_item(business_GUID)
+    #
+    #the above function instantiates the business inventory
 
 class Business_inventory(models.Model):
+    item_guid = models.IntegerField(max_length=100)
+    def get_item(Business_GUID, item_name: item_guid):
+        print(Business_GUID, item_name)
 
 
 class active_job:
